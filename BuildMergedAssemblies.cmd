943000f3ae1f7057bbc1f78ac950badd11e78d69
Iain Ballard <iain.ballard@7digital.com>
2012-12-19 10:23:48 +0000

    More service stack junk

--- BuildMergedAssemblies.cmd
+++ BuildMergedAssemblies.cmd
@@ -3,7 +3,7 @@ mkdir binaries
 mkdir merged
 cd binaries
 
-copy "..\src\SevenDigital.ServiceStack.Merged\bin\Debug\ServiceStack.*.dll" "." /Y
+copy "..\src\SevenDigital.ServiceStack.Merged\bin\Debug\ServiceStack*.dll" "." /Y
 
-..\tools\ILMerge.exe /allowDup /targetplatform:"v4,c:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:..\merged\SevenDigital.ServiceStack.Merged.dll ServiceStack.Common.dll ServiceStack.Interfaces.dll ServiceStack.Text.dll
+..\tools\ILMerge.exe /allowDup /targetplatform:"v4,c:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:..\merged\SevenDigital.ServiceStack.Merged.dll ServiceStack.Interfaces.dll ServiceStack.Common.dll ServiceStack.Text.dll ServiceStack.ServiceInterface.dll ServiceStack.dll ServiceStack.OrmLite.dll
 
