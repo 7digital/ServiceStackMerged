5169e463b806ae95d845e9401953dc8ef9a3b39f
Andres G. Aragoneses <andres@7digital.com>
2012-12-21 12:46:42 +0000

    Include ServiceStack.Logging.Log4Netv1210.dll in the merge hell
    For this, log4net needs to be added in the mix (in the mix to make the
    merged assembly, not to merge it with the rest!)

--- BuildMergedAssemblies.cmd
+++ BuildMergedAssemblies.cmd
@@ -3,7 +3,7 @@ mkdir binaries
 mkdir merged
 cd binaries
 
-copy "..\src\SevenDigital.ServiceStack.Merged\bin\Debug\ServiceStack*.dll" "." /Y
+copy "..\src\SevenDigital.ServiceStack.Merged\bin\Debug\*.dll" "." /Y
 
-..\tools\ILMerge.exe /allowDup /targetplatform:"v4,c:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:..\merged\SevenDigital.ServiceStack.Merged.dll ServiceStack.Interfaces.dll ServiceStack.Common.dll ServiceStack.Text.dll ServiceStack.ServiceInterface.dll ServiceStack.dll ServiceStack.OrmLite.dll
+..\tools\ILMerge.exe /allowDup /targetplatform:"v4,c:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:..\merged\SevenDigital.ServiceStack.Merged.dll ServiceStack.Interfaces.dll ServiceStack.Common.dll ServiceStack.Text.dll ServiceStack.ServiceInterface.dll ServiceStack.dll ServiceStack.OrmLite.dll ServiceStack.Logging.Log4Netv1210.dll
 
